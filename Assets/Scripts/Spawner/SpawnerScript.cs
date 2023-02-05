2efba9436c666f43a708a8c605f4ef160e7036fe
Aleksi A <djoah003@edu.xamk.fi>
2023-02-05 05:17:00 +0200

    Spawn Working

--- Assets/Scripts/Spawner/SpawnerScript.cs
+++ Assets/Scripts/Spawner/SpawnerScript.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class SpawnerScript : MonoBehaviour
 {
 
-    public Rigidbody Enemy = null; //Enemy (e.g Bug), that we want the spawner to spawn.
+    public Rigidbody2D Enemy = null; //Enemy (e.g Bug), that we want the spawner to spawn.
     public int WaveEnemyAmount = 0, AmbushEnemyAmount = 0; //Tells the amount of spawned enemies currently on the map.
     public float WaveCount = 10, AmbushCount = 5; //Changeable amount of enemies. 10 is default.
     public float EnemyMultiplier = 1; //Multiplies the enemy spawn amount.
     public float EnemyIncrement = 0; //Adds to the EnemyMultiplier after each wave.
-    public float x_pos, z_pos; //Modifiable float, that can change the spawn distance from the spawn centre.
+    public float x_pos, y_pos; //Modifiable float, that can change the spawn distance from the spawn centre.
     public float y; //Set default.
     public float SecondsDelay_Wave = 60, SecodsDelay_Ambush = 20;
-    private Vector3 SpawnerPos;
+    private Vector2 SpawnerPos;
 
     public float timer; //Debug timer.
 
@@ -72,8 +72,6 @@ public class SpawnerScript : MonoBehaviour
     private void InstantiateEnemy()
     {
         //Randombly spawns enemy within a set area. The area is set with x_pos & z_pos as offsets from the spawners centre. 
-        Instantiate(Enemy, new Vector3(Random.Range(SpawnerPos.x - x_pos, SpawnerPos.x + x_pos),
-                                              y, //Default y-coordinate spawn for enemy.
-                                              Random.Range(SpawnerPos.z - z_pos, SpawnerPos.z + z_pos)), this.gameObject.transform.rotation);
+        Instantiate(Enemy, new Vector2(Random.Range(SpawnerPos.x-x_pos, SpawnerPos.x + x_pos), Random.Range(SpawnerPos.y - y_pos, SpawnerPos.y + y_pos)), this.gameObject.transform.rotation);
     }
 }
